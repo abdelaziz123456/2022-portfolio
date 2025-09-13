41fdebb5230e6e0a729b5cffcae857e4726adf2b
Abdelaziz <abdelazizmohamed1233@gmail.com>
2025-09-13 13:05:26 +0300

    modify cv link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,38 +1,65 @@
-import React from 'react'
-import './Header.scss'
+import React from "react";
+import "./Header.scss";
 export default function Header() {
   return (
+    <div className="header">
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid  ">
+          <a className="navbar-brand  hovered-text ms-3" href="#">
+            &lt; Abdelaziz &frasl; &gt;
+          </a>
+          <button
+            className="navbar-toggler navbar-dark "
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav  ms-auto ">
+              <li className="nav-item ">
+                <a
+                  className="nav-link active hovered-text"
+                  aria-current="page"
+                  href="#about"
+                >
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link hovered-text" href="#experience">
+                  Experience
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link hovered-text" href="#work">
+                  Work
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link hovered-text" href="#contact">
+                  Contact
+                </a>
+              </li>
 
-    <div className='header'>
-   <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid  ">
-    <a className="navbar-brand  hovered-text ms-3" href="#">&lt; Abdelaziz  &frasl; &gt;</a>
-    <button className="navbar-toggler navbar-dark " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" >
-      <span className="navbar-toggler-icon" ></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav  ms-auto ">
-        <li className="nav-item ">
-          <a className="nav-link active hovered-text" aria-current="page" href="#about">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link hovered-text" href="#experience">Experience</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link hovered-text" href="#work">Work</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link hovered-text" href='#contact'>Contact</a>
-        </li>
-        <li className="nav-item btn-special">
-          <a className="nav-link  " href={process.env.PUBLIC_URL +'cv/abdelaziz mohamed .pdf'} target={'_blank'} download>Resume</a>
-        </li>
-      </ul>
+              <li className="nav-item btn-special">
+                <a
+                  className="nav-link  "
+                  href="https://drive.google.com/file/d/1fDbO3dpHtHvjOgAsgosBHWEnPLiTzcPY/view?usp=sharing"
+                  target={"_blank"}
+                  download
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-    </div>
-
-
-  )
+  );
 }
